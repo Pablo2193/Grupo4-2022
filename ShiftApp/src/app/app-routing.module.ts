668e3dfad69299357d5bf8a437b66761e8440a88
pablonicovillalba2193@gmail.com <pablonicovillalba2193@gmail.com>
2022-10-14 20:15:26 -0300

    Creamos paginas, componetes y las rutas

---
+++ ShiftApp/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { NosotrosComponent } from './Pages/nosotros/nosotros.component';
+import { ECommerceComponent } from './Pages/eCommerce/eCommerce.component';
+import { IngresarComponent } from './Pages/ingresar/ingresar.component';
+import { RegistrarseComponent } from './Pages/registrarse/registrarse.component';
+import { TurneroComponent } from './Pages/turnero/turnero.component';
+
+const routes: Routes = [
+ {path: "", component: AppComponent, pathMatch: "full" },
+ {path:"inicio", component: InicioComponent},
+ {path:"nosotros", component: NosotrosComponent},
+ {path:"eCommerce", component: ECommerceComponent},
+ {path:"ingresar", component: IngresarComponent},
+ {path:"registrarse", component: RegistrarseComponent},
+ {path:"turnero", component: TurneroComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- ShiftApp/src/app/app.module.ts
+++ ShiftApp/src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './Layout/Layout.component';
+import { PagesComponent } from './Pages/Pages.component';
+import { ServiciosComponent } from './Servicios/Servicios.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      LayoutComponent,
+      PagesComponent,
+      PagesComponent,
+      ServiciosComponent
+   ],
   imports: [
     BrowserModule
   ],
