2472849ff5a2499c8da9bdc55fff379808cfe3d8
pablonicovillalba2193@gmail.com <pablonicovillalba2193@gmail.com>
2022-10-17 21:26:51 -0300

    Subo navbar funcionando y login (no se ve aun)

--- ShiftApp/src/app/app.component.spec.ts
+++ ShiftApp/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './Layout/navbar/navbar.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent
       ],
     }).compileComponents();
   });
